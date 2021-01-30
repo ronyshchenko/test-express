8af020417750a63df49120a3bec34e2633c25eaa
ronyshchenko <roma.onyshchenko@gmail.com>
2021-01-30 18:43:23 +0200

    end-commit

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.get('/', function(req, res) {
     res.end(`
     Документация по REST API
 
-Получение вкладки постов (метод: get):/api/posts/list/?numberTab=номер вкладки
+    Получение вкладки постов (метод: get):/api/posts/list/?numberTab=номер вкладки
 Получение поста по id (метод get): /api/posts/:id
 Создание поста (метод post): /api/posts/create/
 Удаление поста (метод delete): /api/posts/delete/:id
-Корректировка поста (метод put): /api/posts/update/:id
+Корректировка поста (метод put): /api/posts/update/:id`);
 });
 
 app.get('/api/posts/list/', (req, res) => {
